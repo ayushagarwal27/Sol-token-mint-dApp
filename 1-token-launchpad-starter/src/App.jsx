71a2d66eb006c13cd215e63a93abbc0c3a328c42
Ayush <ayushagarwal.dev@gmail.com>
2024-09-06 21:00:19 +0530

    add create mint and mint function

--- 1-token-launchpad-starter/src/App.jsx
+++ 1-token-launchpad-starter/src/App.jsx
@@ -1,10 +1,32 @@
 import './App.css'
 import { TokenLaunchpad } from './components/TokenLaunchpad'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import {
+  WalletModalProvider,
+  // WalletDisconnectButton,
+  WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import '@solana/wallet-adapter-react-ui/styles.css';
 
-function App() {
+
+export default function App() {
   return (
-    <TokenLaunchpad></TokenLaunchpad>
+      <div>
+        <ConnectionProvider endpoint={"https://solana-devnet.g.alchemy.com/v2/NXPBVxsNC7ibJue5qAMmlpz-sLPLEcXO"}>
+          <WalletProvider wallets={[]} autoConnect>
+            <WalletModalProvider>
+              <div style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                padding: 20
+              }}>
+                <WalletMultiButton />
+                {/*<WalletDisconnectButton />*/}
+              </div>
+              <TokenLaunchpad></TokenLaunchpad>
+            </WalletModalProvider>
+          </WalletProvider>
+        </ConnectionProvider>
+      </div>
   )
 }
-
-export default App
